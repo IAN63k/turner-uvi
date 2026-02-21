ad5ed8f7c49ca6be4a3b849112611f10b91d57fe
IAN63K <julian63k@gmail.com>
2026-02-21 16:04:38 -0500

    Build: versión de la aplicación v1.0.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         {children}
         <footer className="fixed bottom-0 left-0 w-full pointer-events-none py-2 text-center">
           <span className="text-[10px] font-mono text-muted/40 tracking-wider select-none">
-            Turner v0.1.0 — UVI
+            Turner v1.0.0 — UVI
           </span>
         </footer>
       </body>
